9c70f3338b9347ff57f9b3683ecf1e04168e78e3
James Geneser <jamesgeneser@gmail.com>
2023-10-03 12:44:10 -0600

    added lanes using linear interpolaiton

--- road.js
+++ road.js
@@ -2,7 +2,7 @@ class Road {
   constructor(x, width, laneCount = 3) {
     this.x = x;
     this.width = width;
-    this.laneCount = this.laneCount;
+    this.laneCount = laneCount;
 
     this.left = x - width / 2;
     this.right = x + width / 2;
@@ -17,14 +17,16 @@ class Road {
     ctx.lineWidth = 5;
     ctx.strokeStyle = "white";
 
-    ctx.beginPath();
-    ctx.moveTo(this.left, this.top);
-    ctx.lineTo(this.left, this.bottom);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(this.right, this.top);
-    ctx.lineTo(this.right, this.bottom);
-    ctx.stroke();
+    for (let i = 0; i <= this.laneCount; i++) {
+      const x = lerp(this.left, this.right, i / this.laneCount);
+      ctx.beginPath();
+      ctx.moveTo(x, this.top);
+      ctx.lineTo(x, this.bottom);
+      ctx.stroke();
+    }
   }
+}
+
+function lerp(a, b, t) {
+  return a + (b - a) * t;
 }
